8e34e81a531f3d8e646aa2930a107f87effe8b81
Groundy <44726820+Groundy@users.noreply.github.com>
2021-12-06 22:13:57 +0100

    dodanie przycisku NFC
    dodanie przycisku włączającego i wyłączającego NFC, bez obsługi, dodanie ikony do tego przycisku

--- app/src/main/java/com/example/omega/MainActivity.kt
+++ app/src/main/java/com/example/omega/MainActivity.kt
@@ -5,19 +5,71 @@ import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseApp
 import android.content.Intent
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import androidx.core.graphics.drawable.toBitmap
 import kotlinx.android.synthetic.main.settings_activity.*
 
 class MainActivity: AppCompatActivity() {
 	private lateinit var goQRActivityButton: Button
 	private lateinit var codeField: EditText
-
+	private lateinit var nfcOnOffButton : Button
 	private val QR_SCANNER_ACTIVITY_RET_CODE = 0x101
+
+	@SuppressLint("ResourceType")
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+		FirebaseApp.initializeApp(this)
+		initUIVariables()
+	}
+
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		super.onActivityResult(requestCode, resultCode, data)
+		if(requestCode == QR_SCANNER_ACTIVITY_RET_CODE && resultCode == RESULT_OK){
+			if(data!= null){
+				val returnedCode = data.getIntExtra("codeFromQR",-1)
+				val vailCode = returnedCode in 0..999999
+				if(vailCode) {
+					codeField.setText(returnedCode.toString())
+					processCode(returnedCode)
+				}
+			}
+		}
+	}
+	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+		menuInflater.inflate(R.menu.main_app_menu, menu)
+		return super.onCreateOptionsMenu(menu)
+	}
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+		if(item.itemId == R.id.ConfigurationsTab){
+			val settingsActivityIntent = Intent(this@MainActivity,SettingsActivity::class.java)
+			startActivity(settingsActivityIntent)
+			return true
+		}
+		return false
+	}
+	private fun processCode(code : Int){
+		Utilites.showToast(this,"process: " + code.toString())
+	}
+	private fun initUIVariables(){
+		goQRActivityButton = findViewById(R.id.goToQRScannerButton)
+		codeField = findViewById(R.id.enterCodeField)
+		codeField.requestFocus()
+		goQRActivityButton.setOnClickListener(goQRScannerButtonListener)
+		codeField.addTextChangedListener(codeFieldTextListener)
+		nfcOnOffButton = findViewById(R.id.nfcButton)
+		nfcOnOffButton.setOnClickListener(nfcButtonListener)
+	}
+	private fun test(){
+
+	}
 
 	private val codeFieldTextListener = object : TextWatcher {
 
@@ -40,54 +92,17 @@ class MainActivity: AppCompatActivity() {
 		val qRScannerActivityIntent = Intent(this, QRScannerActivity::class.java)
 		startActivityForResult(qRScannerActivityIntent, QR_SCANNER_ACTIVITY_RET_CODE)
 	}
-
-
-	@SuppressLint("ResourceType")
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
-		FirebaseApp.initializeApp(this)
-		goQRActivityButton = findViewById(R.id.goToQRScannerButton)
-		codeField = findViewById(R.id.enterCodeField)
-		codeField.requestFocus()
-		goQRActivityButton.setOnClickListener(goQRScannerButtonListener)
-		codeField.addTextChangedListener(codeFieldTextListener)
-	}
-
-	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-		super.onActivityResult(requestCode, resultCode, data)
-		if(requestCode == QR_SCANNER_ACTIVITY_RET_CODE && resultCode == RESULT_OK){
-			if(data!= null){
-				val returnedCode = data.getIntExtra("codeFromQR",-1)
-				val vailCode = returnedCode in 0..999999
-				if(vailCode) {
-					codeField.setText(returnedCode.toString())
-					processCode(returnedCode)
-				}
-			}
+	private val nfcButtonListener = View.OnClickListener {
+		val imgFirst = nfcOnOffButton.background.constantState
+		val imgSecond = getDrawable(R.drawable.nfc_on_icon)!!.constantState
+		val isAlreadyTurnedOn = imgFirst == imgSecond
+		if(isAlreadyTurnedOn){
+			//Turn off
+			nfcOnOffButton.setBackgroundResource(R.drawable.nfc_off_icon)
+		}else{
+			nfcOnOffButton.setBackgroundResource(R.drawable.nfc_on_icon)
+			//Turn off
 		}
-	}
-
-	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-		menuInflater.inflate(R.menu.main_app_menu, menu)
-		return super.onCreateOptionsMenu(menu)
-	}
-
-	override fun onOptionsItemSelected(item: MenuItem): Boolean {
-		if(item.itemId == R.id.ConfigurationsTab){
-			val settingsActivityIntent = Intent(this@MainActivity,SettingsActivity::class.java)
-			startActivity(settingsActivityIntent)
-			return true
-		}
-		return false
-	}
-
-	private fun processCode(code : Int){
-		Utilites.showToast(this,"process: " + code.toString())
-	}
-
-	private fun test(){
-
 	}
 
 }
