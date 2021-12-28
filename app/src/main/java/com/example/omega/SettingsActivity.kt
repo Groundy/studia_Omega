0cb6e45231586e26966343a8f426a266d5ff632c
Groundy <44726820+Groundy@users.noreply.github.com>
2021-12-28 23:12:14 +0100

    Task4, dodanie okna wyboru preferowanej metody autoryzacji
    dodanie okna wyboru preferowanej metody autoryzacji

--- app/src/main/java/com/example/omega/SettingsActivity.kt
+++ app/src/main/java/com/example/omega/SettingsActivity.kt
@@ -1,15 +1,27 @@
 package com.example.omega
+import android.app.Dialog
+import android.content.DialogInterface
 import android.nfc.NfcAdapter
 import android.os.Bundle
-import android.widget.Switch
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.select_auth_methode_dialog.*
+
 
 class SettingsActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.settings_activity)
 		fillGuiSettingsWithSavedState()
+
+		val selectAuthMethodeField = findViewById<TextView>(R.id.selectAuthMethodeTextView)
+		val selectAuthMethodeFieldListener = View.OnClickListener {
+			showSelectAuthMethodeDialog()
+		}
+		selectAuthMethodeField.setOnClickListener(selectAuthMethodeFieldListener)
+
 	}
 
 	private fun fillGuiSettingsWithSavedState() {
@@ -22,8 +34,20 @@ class SettingsActivity : AppCompatActivity() {
 			findViewById<Switch>(R.id.turnOnNFCWhenAppStartsSwitch).isVisible = false
 		}
 
-	}
+		val selectAuthMethodeField = findViewById<TextView>(R.id.selectAuthMethodeTextView)
+		val methodeCode = Utilites.readPref_Int(this, R.integer.preferedAuthMethode)
+		var methodeName: String = ""
+		methodeName = when(methodeCode){
+			0 -> getString(R.string.selectAuthMethodeText_pin)
+			1 -> getString(R.string.selectAuthMethodeText_pattern)
+			2 -> getString(R.string.selectAuthMethodeText_finger)
+			else -> getString(R.string.selectAuthMethodeText_pin)
+		}
+		val textToSetOnSelectAuthMethode = getString(R.string.selectAuthMethodeText_base) + methodeName
+		selectAuthMethodeField.text = textToSetOnSelectAuthMethode
 
+
+	}
 	private fun saveResults() {
 		val phoneHasNfc = NfcAdapter.getDefaultAdapter(this) != null
 		if(phoneHasNfc){
@@ -32,7 +56,42 @@ class SettingsActivity : AppCompatActivity() {
 		}
 
 	}
-
+	private fun showSelectAuthMethodeDialog(){
+		val dialog : Dialog = Dialog(this)
+		dialog.setContentView(R.layout.select_auth_methode_dialog)
+		val radioButtonGroup = dialog.selectAuthMethodeButtonGroup
+		val radioButtonGroupListener = RadioGroup.OnCheckedChangeListener { radioButtonGroup, radioButtonId -> // checkedId is the RadioButton selected
+			dialog.dismiss()
+		}
+		val dialogOnDismissListener = DialogInterface.OnDismissListener{
+			var valueCodeToSave = 0
+			var methodeName = ""
+			when(radioButtonGroup.checkedRadioButtonId){
+				dialog.selectAuthMethodeButton_PIN.id ->{
+					methodeName = getString(R.string.selectAuthMethodeText_pin)
+					valueCodeToSave = 0
+				}
+				dialog.selectAuthMethodeButton_patern.id ->{
+					methodeName = getString(R.string.selectAuthMethodeText_pattern)
+					valueCodeToSave = 1
+				}
+				dialog.selectAuthMethodeButton_finger.id ->{
+					methodeName = getString(R.string.selectAuthMethodeText_finger)
+					valueCodeToSave = 2
+				}
+				else ->{
+					methodeName = getString(R.string.selectAuthMethodeText_pin)
+					valueCodeToSave = 0
+				}
+			}
+			Utilites.savePref(this,R.integer.preferedAuthMethode,valueCodeToSave)
+			val textToSetOnWidget = getString(R.string.selectAuthMethodeText_base) + methodeName
+			findViewById<TextView>(R.id.selectAuthMethodeTextView).text = textToSetOnWidget
+		}
+		dialog.setOnDismissListener(dialogOnDismissListener)
+		radioButtonGroup.setOnCheckedChangeListener(radioButtonGroupListener)
+		dialog.show()
+	}
 	override fun onBackPressed() {
 		super.onBackPressed()
 		saveResults()
