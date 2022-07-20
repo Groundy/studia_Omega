59422884bf4ce309d2f5a873f4f47d574c8bdc2e
Groundy <Szczygielski107@gmail.com>
2022-07-20 20:59:51 +0200

    Dodanie funkcji sprawdzania czy timestamp is vaild
    -Dodanie funkcji sprawdzania czy timestamp is vaild

--- app/src/main/java/com.example.omega/OmegaTime.kt
+++ app/src/main/java/com.example.omega/OmegaTime.kt
@@ -3,6 +3,7 @@ package com.example.omega
 import android.util.Log
 import java.lang.Exception
 import java.time.Instant
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
@@ -13,36 +14,11 @@ class OmegaTime {
 			val currentTimeStr = DateTimeFormatter.ISO_INSTANT.format(time)
 			return currentTimeStr
 		}
-		fun miliSecToLeft(endTime : String) : Long? {
-			try{
-				assert(endTime.length == 24)
-				val c = Calendar.getInstance()
-				//val Y = (c.get(Calendar.YEAR)).toString().toLong()
-				//val M = (c.get(Calendar.MONTH) + 1).toString().toLong()//0-11 -> 1-12
-				//val D = (c.get(Calendar.DAY_OF_MONTH)).toString().toLong()
-				val hh = (c.get(Calendar.HOUR_OF_DAY)).toString().toLong()
-				val mm = (c.get(Calendar.MINUTE)).toString().toLong()
-				val ss = (c.get(Calendar.SECOND)).toString().toLong()
-				val zzz = (c.get(Calendar.MILLISECOND)).toString().toLong()
-
-				//val year = time.subSequence(0,4).toString().toLong()
-				//val month = time.subSequence(5,7).toString().toLong()
-				//val day = time.subSequence(8,10).toString().toLong()
-				val hour = endTime.subSequence(11,13).toString().toLong()
-				val minute = endTime.subSequence(14,16).toString().toLong()
-				val sec = endTime.subSequence(17,19).toString().toLong()
-				val miliSec = endTime.subSequence(20,23).toString().toLong()
-
-				var milSecFromNow  = (hour - hh)*3600*1000 + (minute - mm)*60*1000 + (sec - ss)*1000 + (miliSec - zzz)
-				if(milSecFromNow < 0)
-					milSecFromNow += 24*3600*1000//next day
-
-				return milSecFromNow
-			}
-			catch(e : Exception){
-				Log.e(Utilities.TagProduction, "Wrong format of time object string! [$e]")
-				return null
-			}
+		fun timestampIsValid(timestamp : String, minTimeMarginMili : Long = 100) : Boolean{
+			val timeToCheck = Instant.parse(timestamp)
+			val timeCurrent =  Instant.parse(getCurrentTime()).plusMillis(minTimeMarginMili)
+			val isValid = timeToCheck > timeCurrent
+			return isValid
 		}
 	}
 }
