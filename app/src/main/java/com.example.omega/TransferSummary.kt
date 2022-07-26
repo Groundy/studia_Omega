c96654542336f9e3c63388f1e3fc79535297bdd2
Groundy <Szczygielski107@gmail.com>
2022-07-26 22:19:55 +0200

    clean-up w TransferSummary

--- app/src/main/java/com.example.omega/TransferSummary.kt
+++ app/src/main/java/com.example.omega/TransferSummary.kt
@@ -12,31 +12,31 @@ class TransferSummary : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_transfer_summary)
+		getDataFromIntent()
 		setUpGUI()
 	}
 
 	private fun setUpGUI() {
-		val transferDataSerializedField = getString(R.string.TransferSummary_COM_serializedData)
-		val transferDataSerialized = intent.getStringExtra(transferDataSerializedField)
-		transferData = TransferData(transferDataSerialized!!)
-
-		findViewById<TextView>(R.id.transferSummary_amount).text = "${transferData.amount} ${transferData.currency}"
+		val transferAmountText = "${transferData.amount} ${transferData.currency}"
+		findViewById<TextView>(R.id.transferSummary_amount).text = transferAmountText
 		findViewById<TextView>(R.id.transferSummary_receAcc).text = "${transferData.receiverAccNumber}"
 		findViewById<TextView>(R.id.transferSummary_receName).text = "${transferData.receiverName}"
 		findViewById<TextView>(R.id.transferSummary_senderAcc).text = "${transferData.senderAccNumber}"
 		findViewById<TextView>(R.id.transferSummary_title).text = "${transferData.title}"
 
-		findViewById<Button>(R.id.TransferSummary_cancel_Button).setOnClickListener{cancelClicked()}
-		findViewById<Button>(R.id.TransferSummary_auth_Button).setOnClickListener{authClicked()}
+		findViewById<Button>(R.id.TransferSummary_cancel_Button).setOnClickListener{
+			finish()
+		}
+		findViewById<Button>(R.id.TransferSummary_auth_Button).setOnClickListener{
+			val description  = transferData.toDisplayString()
+			ActivityStarter.startAuthActivity(this, description, null)
+		}
 	}
-	private fun cancelClicked(){
-		this.finish()
+	private fun getDataFromIntent(){
+		val transferDataSerializedField = getString(R.string.TransferSummary_COM_serializedData)
+		val transferDataSerialized = intent.getStringExtra(transferDataSerializedField)
+		transferData = TransferData(transferDataSerialized!!)
 	}
-	private fun authClicked(){
-		val description  = transferData.toString()
-		ActivityStarter.startAuthActivity(this, description, null)
-	}
-
 	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 		super.onActivityResult(requestCode, resultCode, data)
 
