ec91674263282a3bd1d0369c7e8b8f3b45783112
Groundy <Szczygielski107@gmail.com>
2022-08-02 16:14:44 +0200

    Przerobienie payment acc z jsona do zmiennych

--- app/src/main/java/com/example/omega/PaymentAccount.kt
+++ app/src/main/java/com/example/omega/PaymentAccount.kt
@@ -3,37 +3,113 @@ package com.example.omega
 import android.util.Log
 import org.json.JSONObject
 import com.example.omega.Utilities.Companion.TagProduction
+import com.example.omega.PaymentAccount.Companion.AccountObjectFields.*
+import com.example.omega.PaymentAccount.Companion.OtherJsonFields.*
 
 class PaymentAccount() {
-    companion object{
+    private companion object{
         enum class BankResponseJsonFields(val text: String){
             AccountObj("account"),
             ResponseHeaderObj("responseHeader");
         }
         enum class AccountObjectFields(val text : String){
             AccountNumber("accountNumber"),
-            //AccountTypeName("accountTypeName"),
+            AccountTypeName("accountTypeName"),
             Currency("currency"),
             AvailableBalance("availableBalance"),
-            //BookingBalance("bookingBalance"),
-            //AccountHolderType("accountHolderType"),
+            BookingBalance("bookingBalance"),
+            AccountHolderType("accountHolderType"),
 
             NameAdressObj("nameAddress"),
-            //AccountTypeObj("accountType"),
-            //PsuRelationsArr("psuRelations"),
-            //BankObj("bank");
-            //Dostępne są bardziej szczególowe info, np.Nazwa banku
+            AccountTypeObj("accountType"),
+            PsuRelationsArray("psuRelations"),
+            BankObj("bank");
         }
-        enum class NameClassObjFields(val text : String){
-            Value("value")
+	    enum class BankObjFields(val text : String){
+		    Address("address"),
+		    Name("name"),
+		    BicOrSwift("bicOrSwift");
+		}
+        enum class BicOrSwiftType(val text: String){
+            Bic("bic"), Swift("swift"), Undefined("Undefined");
+	        companion object{
+		        fun fromStr(text: String) : BicOrSwiftType{
+			        return when(text){
+				        "bic" ->Bic
+				        "swift" -> Swift
+				        else -> Undefined
+			        }
+		        }
+			}
+
         }
+	    enum class OtherJsonFields(val text : String){
+		    Description("description"),
+		    Value("value"),
+		    TypeOfRelation("typeOfRelation");
+		}
     }
 
-    private var contentJson : JSONObject? = null
+    private var accountNumber : String? = null
+    private var ownerName : String? = null
+    private var accountDescription : String? = null
+    private var accountTypeName : String? = null
+    private var currency : String? = null
+    private var availableBalance : Double? = null
+    private var bookingBalance : Double? = null
+    private var accountHolder  : String? = null
+	private var psuRelations : String? = null
 
-    constructor(jsonObject: JSONObject) : this() {
-        contentJson = try {
-            jsonObject.getJSONObject(BankResponseJsonFields.AccountObj.text)
+    private var bankName : String? = null
+    private var bankAdress : String? = null
+    private var bankType : BicOrSwiftType? = null
+
+    constructor(jsonObjectResponse: JSONObject) : this() {
+        try {
+	        val accountDetailsObj = jsonObjectResponse.getJSONObject(BankResponseJsonFields.AccountObj.text)
+	        val ownerNameJsonArray = accountDetailsObj.getJSONObject(NameAdressObj.text).getJSONArray(OtherJsonFields.Value.text)
+	        var ownerNameTmp = String()
+	        for (i in 0 until ownerNameJsonArray.length()){
+		        ownerNameTmp = ownerNameTmp.plus(ownerNameJsonArray[i]).plus(" ")
+	        }
+	        val bankObj = accountDetailsObj.getJSONObject(BankObj.text)
+
+	        val psuRelationArrTmp = accountDetailsObj.getJSONArray(PsuRelationsArray.text)
+	        val size = psuRelationArrTmp.length()
+	        val psuRealtionsTmp = if(size >= 1){
+		        val psuRelationsObj = psuRelationArrTmp.getJSONObject(0)
+		        psuRelationsObj.getString(TypeOfRelation.text)
+			}
+	        else
+				String()
+
+	        val bankAdressObjTmp = bankObj.getJSONObject(BankObjFields.Address.text)
+	        val bankAdressArrayTmp = bankAdressObjTmp.getJSONArray(Value.text)
+	        var bankAdressTmp = String()
+	        val size2 = bankAdressArrayTmp.length()
+	        for (i in 0 until size2){
+		        bankAdressTmp = bankAdressTmp.plus(bankAdressArrayTmp.get(i))
+		        if(i != size2 - 1 )
+			        bankAdressTmp = bankAdressTmp.plus(", ")
+			}
+
+
+
+	        ownerName = ownerNameTmp
+            accountNumber = accountDetailsObj.getString(AccountNumber.text)
+            accountDescription = accountDetailsObj.getJSONObject(AccountTypeObj.text).getString(Description.text)
+            accountTypeName = accountDetailsObj.getString(AccountTypeName.text)
+            currency = accountDetailsObj.getString(Currency.text)
+            availableBalance = accountDetailsObj.getDouble(AvailableBalance.text)
+            bookingBalance = accountDetailsObj.getDouble(BookingBalance.text)
+            accountHolder = accountDetailsObj.getString(AccountHolderType.text)
+	        psuRelations = psuRealtionsTmp
+
+
+            bankName  = bankObj.getString(BankObjFields.Name.text)
+            bankAdress = bankAdressTmp
+	        val bicOrSwiftStr = bankObj.getString(BankObjFields.BicOrSwift.text)
+            bankType = BicOrSwiftType.fromStr(bicOrSwiftStr)
         }catch(e : Exception){
             Log.e(TagProduction,"[constructor(json)/${this.javaClass.name}], Error Wrong json struct!")
             null
@@ -50,59 +126,31 @@ class PaymentAccount() {
         return "[$availableBalance $currency]  $accNumber"
     }
     fun getCurrencyOfAccount() : String{
-        if(this.contentJson == null){
+        if(currency == null){
             Log.e(TagProduction, "[getCurrencyOfAccount/${this.javaClass.name}] json in payment acc is null")
             return "Null"
         }
-
-        return try {
-            contentJson!!.getString(AccountObjectFields.Currency.text).toString()
-        }catch (e :Exception){
-            Log.e(TagProduction, "[getCurrencyOfAccount/${this.javaClass.name}] wrong Json Struct")
-            "Null"
-        }
+		return currency as String
     }
     fun getBalanceOfAccount() : Double?{
-        if(this.contentJson == null){
-            Log.e(TagProduction, "[getBalanceOfAccount/${this.javaClass.name}] json in payment acc is null")
+        if(availableBalance == null){
+            Log.e(TagProduction, "[getBalanceOfAccount/${this.javaClass.name}] is null")
             return null
         }
-
-        return try {
-            contentJson!!.getString(AccountObjectFields.AvailableBalance.text).toDouble()
-        }catch (e :Exception){
-            Log.e(TagProduction, "[getBalanceOfAccount/${this.javaClass.name}] wrong Json Struct")
-            null
-        }
+	    return availableBalance
     }
     fun getAccNumber() : String{
-        if(this.contentJson == null){
-            Log.e(TagProduction, "[getAccNumber/${this.javaClass.name}] json in payment acc is null")
+        if(accountNumber == null){
+            Log.e(TagProduction, "[getAccNumber/${this.javaClass.name}] s null")
             return "Null"
         }
-
-        return try {
-            contentJson!!.getString(AccountObjectFields.AccountNumber.text).toString()
-        }catch (e :Exception){
-            Log.e(TagProduction, "[getAccNumber/${this.javaClass.name}] wrong Json Struct")
-            "Null"
-        }
+		return accountNumber as String
     }
 	fun getOwnerName(): String{
-        if(this.contentJson == null){
-            Log.e(TagProduction, "[getOwnerName/${this.javaClass.name}] json in payment acc is null")
-            return "Null"
-        }
-        return try {
-            val nameObj = contentJson!!.getJSONObject(AccountObjectFields.NameAdressObj.text)
-            val nameArray = nameObj.getJSONArray(NameClassObjFields.Value.text)
-            var name = String()
-            for (i in 0 until nameArray.length())
-                name = name.plus(nameArray[i])
-            name
-        }catch (e :Exception){
-            Log.e(TagProduction, "[getOwnerName/${this.javaClass.name}] wrong Json Struct")
-            "Null"
-        }
+		if(ownerName == null){
+			Log.e(TagProduction, "[getOwnerName/${this.javaClass.name}] is null")
+			return "null"
+		}
+		return ownerName as String
 	}
 }
