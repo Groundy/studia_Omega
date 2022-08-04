8e0c022acd8665a35e28f956d5af73c3b29f799a
Groundy <Szczygielski107@gmail.com>
2022-08-04 20:32:59 +0200

    Dodanie dialogu czekania

---
+++ app/src/main/java/com/example/omega/WaitingDialog.kt
@@ -0,0 +1,38 @@
+package com.example.omega
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
+class WaitingDialog {
+	private var dialog : AlertDialog
+
+	constructor(callerActivity: Activity, resId : Int){
+		val infalter = callerActivity.layoutInflater
+		val dialogView = infalter.inflate(R.layout.loading_dialog,null)
+		val bulider = AlertDialog.Builder(callerActivity)
+		bulider.setView(dialogView)
+		bulider.setCancelable(false)
+		dialog = bulider.create()
+		val str = callerActivity.resources.getString(resId)
+		CoroutineScope(Main).launch {
+			val textView = dialog.findViewById<TextView>(R.id.WaitingDial_textView)
+			textView.text = str
+		}
+		dialog.show()
+	}
+	fun changeText(callerActivity: Activity, resId : Int){
+		val str = callerActivity.resources.getString(resId)
+		CoroutineScope(Main).launch {
+			val textView = dialog.findViewById<TextView>(R.id.WaitingDial_textView)
+			textView.text = str
+		}
+	}
+	fun hide(){
+		dialog.dismiss()
+	}
+}
