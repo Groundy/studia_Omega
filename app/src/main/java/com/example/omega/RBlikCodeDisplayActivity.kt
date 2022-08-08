c1534d4d1facba71afa90dbc799cbfeb9d18c89f
Groundy <Szczygielski107@gmail.com>
2022-08-08 21:06:17 +0200

    Dodanie Timera do display  activity

--- app/src/main/java/com/example/omega/RBlikCodeDisplayActivity.kt
+++ app/src/main/java/com/example/omega/RBlikCodeDisplayActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.zxing.WriterException
 import android.graphics.Bitmap
+import android.os.CountDownTimer
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
@@ -13,12 +14,28 @@ import androidmads.library.qrgenearator.QRGContents
 import androidmads.library.qrgenearator.QRGEncoder
 import androidx.core.graphics.drawable.toBitmap
 import com.example.omega.Utilities.Companion.TagProduction
+import java.util.*
+import kotlin.math.floor
 
 class RBlikCodeDisplayActivity : AppCompatActivity() {
 	private lateinit var imgWidget : ImageView
 	private lateinit var codeDisplayField : TextView
+	private lateinit var data : ServerSetCodeResponse
+	private lateinit var timer : CountDownTimer
 
 	@SuppressLint("UseCompatLoadingForDrawables")
+	override fun onResume() {
+		super.onResume()
+		val timeLeft = OmegaTime.getSecondsToStampExpiration(data.timestamp,0)
+		if(timeLeft <0 ){
+			finish()
+		}
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		timer.cancel()
+	}
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_blik_code_display)
@@ -38,6 +55,7 @@ class RBlikCodeDisplayActivity : AppCompatActivity() {
 			qrCodeBitmap = resources.getDrawable(R.drawable.ico_failure, null).toBitmap()
 		}
 		imgWidget.setImageBitmap(qrCodeBitmap)
+		startTimer()
 	}
 
 	private fun generateQRCodeImg(code: Int) : Bitmap?{
@@ -71,4 +89,32 @@ class RBlikCodeDisplayActivity : AppCompatActivity() {
 			finish()
 		}
 	}
+	private fun startTimer(){
+		val secondsLeft = OmegaTime.getSecondsToStampExpiration(data.timestamp, 0)
+		if(secondsLeft < 0){
+			ActivityStarter.startOperationResultActivity(this@RBlikCodeDisplayActivity, R.string.Result_GUI_CODE_EXPIRED)
+			setResult(RESULT_CANCELED)
+			return
+		}
+		val timerView = findViewById<TextView>(R.id.BLIKDISPLAY_timer_textView)
+		timer = object: CountDownTimer(secondsLeft*1000, 1000) {
+			override fun onTick(millisUntilFinished: Long) {
+				val secondsToLeft = floor((millisUntilFinished / 1000).toDouble())
+				var minutesToDisplay = (floor(secondsToLeft / 60).toInt()).toString()
+				if(minutesToDisplay.length == 1)
+					minutesToDisplay = "0$minutesToDisplay"
+				var secondsToDisplay = (secondsToLeft % 60).toInt().toString()
+				if(secondsToDisplay.length == 1)
+					secondsToDisplay = "0$secondsToDisplay"
+				timerView.text = "$minutesToDisplay:$secondsToDisplay"
+			}
+
+			override fun onFinish() {
+				ActivityStarter.startOperationResultActivity(this@RBlikCodeDisplayActivity, R.string.Result_GUI_CODE_EXPIRED)
+				setResult(RESULT_CANCELED)
+			}
+		}
+		timer.start()
+	}
 }
+
